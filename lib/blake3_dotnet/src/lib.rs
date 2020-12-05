b1f84c08976c4358a3e9f7785689dc416b7f61e0
Alexandre Mutel <alexandre_mutel@live.com>
2020-12-05 19:25:16 +0100

    Add blake3_reset and blake3_update_join

--- lib/blake3_dotnet/src/lib.rs
+++ lib/blake3_dotnet/src/lib.rs
@@ -12,6 +12,15 @@ pub extern fn blake3_delete(hasher: *mut blake3::Hasher) {
   unsafe{ Box::from_raw(hasher) };
 }
 
+// Resets Blake3 hasher
+#[no_mangle]
+pub extern fn blake3_reset(
+  hasher: *mut blake3::Hasher)
+{
+  let hasher = unsafe { &mut *hasher };
+  hasher.reset();  
+}
+
 // Updates Blake3 hash with data
 #[no_mangle]
 pub extern fn blake3_update(
@@ -24,9 +33,31 @@ pub extern fn blake3_update(
   hasher.update(slice);  
 }
 
+// Updates Blake3 hash with data
+#[cfg(feature = "rayon")]
+#[no_mangle]
+pub extern fn blake3_update_with_join(
+  hasher: *mut blake3::Hasher,
+  ptr: *const u8,
+  size: libc::size_t)
+{
+  let hasher = unsafe { &mut *hasher };
+  let slice = unsafe { std::slice::from_raw_parts(ptr as *const u8, size as usize) };
+  hasher.update_with_join::<blake3::join::RayonJoin>(slice);  
+}
+
+// // Finalize the hash and put the result into output.
+// #[no_mangle]
+// pub extern fn blake3_finalize(
+//   hasher: *mut blake3::Hasher) -> blake3::Hash
+// {
+//   let hasher = unsafe { &mut *hasher };
+//   return hasher.finalize();
+// }
+
 // Finalize the hash and put the result into output.
 #[no_mangle]
-pub extern fn blake3_finalize(
+pub extern fn blake3_finalize_xof(
   hasher: *mut blake3::Hasher,
   ptr: *mut u8,
   size: libc::size_t)
