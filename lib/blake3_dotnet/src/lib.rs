869215c84339e3e51187a606579418979e68432c
Alexandre Mutel <alexandre_mutel@live.com>
2020-12-06 06:25:20 +0100

    Add methods supporting hash directly

--- lib/blake3_dotnet/src/lib.rs
+++ lib/blake3_dotnet/src/lib.rs
@@ -21,6 +21,17 @@ pub extern fn blake3_reset(
   hasher.reset();  
 }
 
+// Blake3::hash
+#[no_mangle]
+pub extern fn blake3_hash(
+  ptr: *const u8,
+  size: libc::size_t,
+  ptr_out: *const u8)
+{
+  let slice = unsafe { std::slice::from_raw_parts(ptr as *const u8, size as usize) };
+  unsafe { *(ptr_out as *mut blake3::Hash) = blake3::hash(slice); }
+}
+
 // Updates Blake3 hash with data
 #[no_mangle]
 pub extern fn blake3_update(
@@ -46,23 +57,24 @@ pub extern fn blake3_update_with_join(
   hasher.update_with_join::<blake3::join::RayonJoin>(slice);  
 }
 
-// // Finalize the hash and put the result into output.
-// #[no_mangle]
-// pub extern fn blake3_finalize(
-//   hasher: *mut blake3::Hasher) -> blake3::Hash
-// {
-//   let hasher = unsafe { &mut *hasher };
-//   return hasher.finalize();
-// }
+// Finalize to a 32 byte hash
+#[no_mangle]
+pub extern fn blake3_finalize(
+  hasher: *mut blake3::Hasher,
+  ptr_out: *const u8)
+{
+  let hasher = unsafe { &mut *hasher };
+  unsafe { *(ptr_out as *mut blake3::Hash) = hasher.finalize(); }
+}
 
 // Finalize the hash and put the result into output.
 #[no_mangle]
 pub extern fn blake3_finalize_xof(
   hasher: *mut blake3::Hasher,
-  ptr: *mut u8,
+  ptr_out: *mut u8,
   size: libc::size_t)
 {
   let hasher = unsafe { &mut *hasher };
-  let slice = unsafe { std::slice::from_raw_parts_mut(ptr as *mut u8, size as usize) };
+  let slice = unsafe { std::slice::from_raw_parts_mut(ptr_out as *mut u8, size as usize) };
   hasher.finalize_xof().fill(slice);
 }
